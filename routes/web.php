35a60940a7ebb07613dd05bd756ff65a8a8e8d57
sadeghbiglar <sbmail555@gmail.com>
2026-02-07 01:25:48 +0330

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Livewire\Provinces\ProvinceIndex;
-use App\Livewire\Cities\CityIndex;
-use App\Livewire\UnitTypes\UnitTypeIndex;
-use App\Livewire\Units\UnitIndex;
-use App\Livewire\Units\UnitTree;
-use App\Livewire\Users\UserIndex;
-use App\Livewire\Tickets\CreateTicket;
-use App\Livewire\Tickets\TicketInbox;
-
-Route::get('/inbox-tickets', TicketInbox::class)->name('tickets.inbox');
-
-Route::get('/tickets/new', CreateTicket::class)->name('tickets.create');
-
-Route::get('/users', UserIndex::class);
-
-Route::get('/units/tree', UnitTree::class);
-
-Route::get('/units', UnitIndex::class);
-
-Route::get('/unit-types', UnitTypeIndex::class);
-
-Route::get('/cities', CityIndex::class);
-
-Route::get('/provinces', ProvinceIndex::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
