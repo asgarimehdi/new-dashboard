60a7222f75ee26485a68e1758a5c78c78cb0368b
sadeghbiglar <sbmail555@gmail.com>
2026-02-08 12:48:38 +0330

    update routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Livewire\Provinces\ProvinceIndex;
+use App\Livewire\Cities\CityIndex;
+use App\Livewire\UnitTypes\UnitTypeIndex;
+use App\Livewire\Units\UnitIndex;
+use App\Livewire\Units\UnitTree;
+use App\Livewire\Users\UserIndex;
+use App\Livewire\Tickets\CreateTicket;
+use App\Livewire\Tickets\TicketInbox;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,5 +23,26 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::get('/inbox-tickets', TicketInbox::class)->name('tickets.inbox');
+
+Route::get('/tickets/new', CreateTicket::class)->name('tickets.create');
+
+Route::get('/users', UserIndex::class);
+
+Route::get('/units/tree', UnitTree::class);
+
+Route::get('/units', UnitIndex::class);
+
+Route::get('/unit-types', UnitTypeIndex::class);
+
+Route::get('/cities', CityIndex::class);
+
+Route::get('/provinces', ProvinceIndex::class);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 require __DIR__.'/auth.php';
